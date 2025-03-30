0ff7b782a6465b0e8d3b22de69f50e00a03bbdd6
icontaylore <icontaylore@icloud.com>
2025-03-30 21:48:07 +0400

    feat and fix:пофиксил всё с докер композ

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,1 +1,31 @@
 package repository
+
+import "gorm.io/gorm"
+
+type RepoInterface interface {
+	Create() error
+	Read() error
+	Update() error
+	Delete() error
+}
+
+type ProductRepository struct {
+	db *gorm.DB
+}
+
+func NewProduct() RepoInterface {
+
+}
+
+func (p *ProductRepository) Create() {
+
+}
+
+func (p *ProductRepository) Read()  {
+
+}
+
+func (p *ProductRepository) ()  {
+
+}
+func (p *ProductRepository) () {}
