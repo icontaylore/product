1928e9836dbe7a493d850907bd7a03233213aaac
icontaylore <icontaylore@icloud.com>
2025-04-01 00:18:45 +0400

    final-fix-day:добавил кафка, есть небольшой вопрос по ней

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -5,8 +5,12 @@ import (
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"log"
+	"os"
+	"os/signal"
 	"product/internal/config"
+	"product/internal/infrastructure/kafka"
 	"product/internal/infrastructure/models"
+	"syscall"
 )
 
 func main() {
@@ -23,10 +27,22 @@ func main() {
 		logger.Fatal("open db: ошибка при открытии соединения с бд")
 	}
 	logger.Println("open db: успешное подключение к бд")
-	// если нету таблицы, создаём её
+	// если нет таблицы, создаём её
 	if err = db.AutoMigrate(&models.Product{}); err != nil {
 		logger.Println("open db: ошибка с применением миграции")
 	}
 	fmt.Println("успешно")
 
+	// kafka
+	topic := "dbz.public.products"
+	logger.Println("Запуск потребителя для топика:", topic)
+	go kafka.StartConsumer(topic)
+
+	// Создаем канал для получения сигналов о завершении работы программы
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM) // Перехватываем сигналы завершения
+
+	// Ожидаем сигнала для завершения работы
+	<-stop
+	log.Println("Завершение работы программы.")
 }
--- internal/infrastructure/kafka/consumer.go
+++ internal/infrastructure/kafka/consumer.go
@@ -1,1 +1,66 @@
 package kafka
+
+import (
+	"fmt"
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+	"log"
+	"os"
+	"os/signal"
+	"product/internal/infrastructure/models"
+	"syscall"
+)
+
+type KafkaMessage struct {
+	Payload struct {
+		Op     string          `json:"op"`
+		Before *models.Product `json:"before"`
+		After  *models.Product `json:"after"`
+	} `json:"payload"`
+}
+
+type Product struct {
+	ID            int     `json:"id"`
+	Name          string  `json:"name"`
+	Description   *string `json:"description,omitempty"` // mb null
+	Price         string  `json:"price"`
+	StockQuantity int64   `json:"stock_quantity"`
+}
+
+func StartConsumer(topic string) {
+	conf := &kafka.ConfigMap{
+		"bootstrap.servers": "localhost:9093",
+		"group.id":          "product-consumer",
+		"auto.offset.reset": "earliest",
+	}
+
+	consumer, err := kafka.NewConsumer(conf)
+	if err != nil {
+		log.Fatal("kafka:not create cons..")
+	}
+	defer consumer.Close()
+
+	consumer.SubscribeTopics([]string{topic}, nil)
+
+	log.Println("kafka:consumer запущен")
+
+	sigchan := make(chan os.Signal)
+	signal.Notify(sigchan, syscall.SIGINT, syscall.SIGTERM)
+
+	for {
+		select {
+		case sig := <-sigchan:
+			fmt.Printf("kafka:signal %v операционной системы off", sig)
+			return
+		default:
+			msg, err := consumer.ReadMessage(-1)
+			if err == nil {
+				handleMessage(msg.Value)
+			} else {
+				log.Printf("Error reading message: %v\n", err)
+			}
+		}
+	}
+}
+func handleMessage(msg []byte) {
+	fmt.Printf("kafka:принято - %s\n", string(msg))
+}
