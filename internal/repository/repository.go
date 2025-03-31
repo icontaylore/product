9b66d6c4187507eb6aa018589d572c6faf1daf92
icontaylore <icontaylore@icloud.com>
2025-03-31 23:12:10 +0400

    fix:доделал CRUD

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,31 +1,69 @@
 package repository
 
-import "gorm.io/gorm"
+import (
+	"fmt"
+	"gorm.io/gorm"
+	"product/internal/infrastructure/models"
+)
 
 type RepoInterface interface {
-	Create() error
-	Read() error
-	Update() error
-	Delete() error
+	Create(product *models.Product) error
+	Read(id uint) error
+	Update(product *models.Product) error
+	Delete(id uint) error
 }
 
 type ProductRepository struct {
 	db *gorm.DB
 }
 
-func NewProduct() RepoInterface {
-
+func NewRepoInterface(db *gorm.DB) RepoInterface {
+	return &ProductRepository{db: db}
 }
 
-func (p *ProductRepository) Create() {
-
+func (p *ProductRepository) Create(product *models.Product) error {
+	result := p.db.Create(product)
+	if result.Error != nil {
+		return fmt.Errorf("repo:не удалось создать продукт")
+	}
+	return nil
 }
 
-func (p *ProductRepository) Read()  {
+func (p *ProductRepository) Read(id uint) error {
+	var prod *models.Product
+	result := p.db.First(&prod, id)
+	if result.Error != nil {
+		if result.Error == gorm.ErrRecordNotFound {
+			return fmt.Errorf("repo:продукт с таким id не найден")
+		}
+		return fmt.Errorf("repo:невозможно вытащить id")
+	}
 
+	return nil
 }
 
-func (p *ProductRepository) ()  {
-
+func (p *ProductRepository) Update(product *models.Product) error {
+	result := p.db.Save(product)
+	if result.Error != nil {
+		return fmt.Errorf("repo:не удалось обновить продукт")
+	}
+	return nil
 }
-func (p *ProductRepository) () {}
+func (p *ProductRepository) Delete(id uint) error {
+	var productDel *models.Product
+
+	result := p.db.First(&productDel, id)
+	if result.Error != nil {
+		if result.Error == gorm.ErrRecordNotFound {
+			return fmt.Errorf("repo:продукт с таким id не найден и не удален")
+		}
+		return fmt.Errorf("repo:не удалось найти продукт с таким id")
+	}
+
+	deleteResult := p.db.Delete(productDel)
+	if deleteResult.Error != nil {
+		return fmt.Errorf("не удалось удалить продукт: %w", deleteResult.Error)
+	}
+
+	return nil
+}
