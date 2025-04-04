7c721aa38a6358d4035d70bd81c35b5e8d5f3ff2
icontaylore <icontaylore@icloud.com>
2025-04-04 22:13:35 +0400

    fix:relog kafka + debezium

--- internal/infrastructure/kafka/config.go
+++ internal/infrastructure/kafka/config.go
@@ -2,27 +2,19 @@ package kafka
 
 import (
 	"github.com/IBM/sarama"
-	"product/internal/infrastructure/elasticsearch"
 )
 
 type KafkaSettings struct {
-	ElasticService *elasticsearch.Service
-	Consumer       sarama.Consumer
-	Worker         uint
-	Topic          string
-	Host           string
-	MessageChan    <-chan *sarama.ConsumerMessage
-	PartitionCons  sarama.PartitionConsumer // контроль закрытия
+	Consumer      sarama.Consumer
+	Addres        []string
+	Topic         string
+	Message       <-chan *sarama.ConsumerMessage
+	PartitionCons sarama.PartitionConsumer
 }
 
-func (k *KafkaSettings) SetElasticService(service *elasticsearch.Service) {
-	k.ElasticService = service
-}
-func GoConfigure(top, host string, worker uint) *KafkaSettings {
+func KafkaGetConfig(arr []string, s string) *KafkaSettings {
 	return &KafkaSettings{
-		Worker:      worker,
-		Topic:       top,
-		Host:        host,
-		MessageChan: make(chan *sarama.ConsumerMessage),
+		Addres: arr,
+		Topic:  s,
 	}
 }
